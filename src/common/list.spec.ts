6ada3ef938e5a361100105b741a1f7d4b6228bab
Kevin Peters <kwpeters@ra.rockwell.com>
2016-05-31 20:40:48 -0400

    Added List operations.

--- src/common/list.spec.ts
+++ src/common/list.spec.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../typings/index.d.ts" />
 
 import * as test from "tape";
-import {List} from "./list";
+import {List, Iterator, MoveIteratorRetType} from "./list";
 
 test("List", {}, function (t: test.Test): void {
 
@@ -10,5 +10,210 @@ test("List", {}, function (t: test.Test): void {
         t.ok(list);
         t.end();
     });
+
+    t.test("initially length should be 0",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            t.equal(list.length, 0);
+            t.end();
+        }
+    );
+
+
+    t.test("isEmpty() should return true for an empty list",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            t.equal(list.length, 0);
+            t.equal(list.isEmpty, true);
+            t.end();
+        }
+    );
+
+
+    t.test("isEmpty() should return false for a non-empty list",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            list.push(1);
+            t.equal(list.length, 1);
+            t.equal(list.isEmpty, false);
+            t.end();
+        }
+    );
+
+
+    t.test("begin(), end() and value() can be used to iterate values in a List",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            list.push(1);
+            list.push(2);
+            list.push(3);
+            
+            let expected: number = 1;
+            
+            for (let it: Iterator<number> = list.begin(); !it.equals(list.end()); it.next()) {
+                t.equal(it.value, expected);
+                ++expected;
+            }
+            
+            t.end();
+        }
+    );
+
+    t.test("push() should add an item to the end of list",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            list.push(1);
+            t.equal(list.length, 1);
+            
+            // Should allow chaining.
+            list.push(2).push(3);
+            t.equal(list.length, 3);
+            
+            t.end();
+        }
+    );
+
+
+    t.test("pop() should remove an item from the end of the list",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            list.push(1).push(2).push(3);
+            t.equal(list.length, 3);
+
+            t.equal(list.pop(), 3);
+            t.equal(list.pop(), 2);
+            t.equal(list.pop(), 1);
+
+            t.end();
+        }
+    );
+
+
+    t.test("remove() should remove the specified node",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            list.push(1).push(2).push(3);
+
+            let itRemove: Iterator<number> = list.begin();
+            itRemove.next();
+            list.remove(itRemove);
+
+            t.equal(list.length, 2);
+            t.equal(list.pop(), 3);
+            t.equal(list.pop(), 1);
+
+            t.end();
+        }
+    );
+
+
+    t.test("getAt() should return the value at the specified index",
+        function (t: test.Test): void {
+            const list: List<number> = new List<number>();
+            list.push(1).push(2).push(3);
+
+            t.equal(list.getAt(0), 1);
+            t.equal(list.getAt(1), 2);
+            t.equal(list.getAt(2), 3);
+            t.end();
+        }
+    );
+
+
+    t.test("ForwardIterator",
+        function (t: test.Test): void {
+
+            t.test("can traverse a 0-length list",
+                function (t: test.Test): void {
+                    const list: List<number> = new List<number>();
+                    const it: Iterator<number> = list.begin();
+                    t.assert(it.next().done);
+                    t.end();
+                }
+            );
+            
+            
+            t.test("Is equal() to another iterator that is pointing at the same node",
+                function (t: test.Test): void {
+                    const list: List<number> = new List<number>();
+                    list.push(1);
+                    t.equal(list.length, 1);
+
+                    const it1: Iterator<number> = list.begin();
+                    const it2: Iterator<number> = list.begin();
+
+                    t.assert(it1.equals(it2));
+                    t.end();
+                }
+            );
+
+
+            t.test("is not euqal() to another iterator pointing at a different node",
+                function (t: test.Test): void {
+                    const list: List<number> = new List<number>();
+                    list.push(1);
+                    t.equal(list.length, 1);
+
+                    const it1: Iterator<number> = list.begin();
+                    const it2: Iterator<number> = list.end();
+
+                    t.assert(!it1.equals(it2));
+                    t.end();
+                }
+            );
+
+
+            t.test("can traverse a 1-element list",
+                function (t: test.Test): void {
+                    const list: List<number> = new List<number>();
+                    list.push(1);
+                    t.equal(list.length, 1);
+                    
+                    const it: Iterator<number> = list.begin();
+                    
+                    let res: MoveIteratorRetType = it.next();
+                    t.equal(res.done, false);
+                    t.equal(res.value, 1);
+
+                    res = it.next();
+                    t.equal(res.done, true);
+                    
+                    t.end();
+                }
+            );
+
+
+            t.test("can traverse a 3-element list",
+                function (t: test.Test): void {
+                    const list: List<number> = new List<number>();
+                    list.push(1);
+                    list.push(2);
+                    list.push(3);
+                    t.equal(list.length, 3);
+
+                    const it: Iterator<number> = list.begin();
+
+                    let res: MoveIteratorRetType = it.next();
+                    t.equal(res.done, false);
+                    t.equal(res.value, 1);
+
+                    res = it.next();
+                    t.equal(res.done, false);
+                    t.equal(res.value, 2);
+
+                    res = it.next();
+                    t.equal(res.done, false);
+                    t.equal(res.value, 3);
+
+                    res = it.next();
+                    t.equal(res.done, true);
+
+                    t.end();
+                }
+            );
+
+
+        }
+    );
 
 });
--- src/common/list.ts
+++ src/common/list.ts
@@ -1,7 +1,174 @@
 /// <reference path="../../typings/index.d.ts" />
 
-export class List<T> {
-    
-    constructor() {}
-    
+
+export class DLNode<ValueType> {
+    public prev: DLNode<ValueType>;
+    public next: DLNode<ValueType>;
+    public value: ValueType;
 }
+
+
+export class List<ValueType> {
+
+    private _end: DLNode<ValueType>;
+
+    constructor() {
+        this._end = new DLNode<ValueType>();
+        this._end.prev = this._end;
+        this._end.next = this._end;
+        this._end.value = undefined;
+    }
+
+
+    public get length(): number {
+        let len: number = 0;
+        let curNode: DLNode<ValueType> = this._end;
+        
+        do {
+            curNode = curNode.next;
+            ++len;
+        } while (curNode !== this._end);
+        
+        return len - 1;
+    }
+
+
+    /**
+     * Determines (in constant time) whether this list is empty.
+     * @returns {boolean} true if this list is empty.  false otherwise.
+     */
+    public get isEmpty(): boolean {
+        // This list is empty if the first node in the list (this._end.next)
+        // points to the end.
+        return this._end.next === this._end;
+    }
+
+
+    public begin(): Iterator<ValueType> {
+        return new Iterator(this._end.next, this._end);
+    }
+
+
+    public end(): Iterator<ValueType> {
+        return new Iterator(this._end, this._end);
+    }
+
+
+    public push(value: ValueType): List<ValueType> {
+        const newNode: DLNode<ValueType> = new DLNode<ValueType>();
+        
+        // Setup the new node.
+        newNode.value = value;
+        newNode.next = this._end;
+        newNode.prev = this._end.prev;
+        
+        // Splice the new node into the list.
+        newNode.prev.next = newNode;
+        this._end.prev = newNode;
+
+        // Return this to allow chaining.
+        return this;
+    }
+
+
+    public pop(): ValueType {
+        const val: ValueType = this._end.prev.value;
+
+        // Remove the node from the list.
+        this.removeNode(this._end.prev);
+
+        return val;
+    }
+
+
+    public remove(it: Iterator<ValueType>): Iterator<ValueType> {
+        const nextNode: DLNode<ValueType> = this.removeNode(it._getDLNode());
+        return new Iterator(nextNode, this._end);
+    }
+
+
+    public getAt(index: number): ValueType {
+        const it: Iterator<ValueType> = this.begin();
+
+        for (let curIndex: number = 0; curIndex < index; ++curIndex) {
+            it.next();
+        }
+
+        return it.value;
+    }
+
+
+    private removeNode(removeNode: DLNode<ValueType>): DLNode<ValueType> {
+        const prevNode: DLNode<ValueType> = removeNode.prev;
+        const nextNode: DLNode<ValueType> = removeNode.next;
+
+        // Remove the element from the list.
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+
+        return nextNode;
+    }
+
+}
+
+
+/**
+ * Describes the return type from an iterator's next() method.
+ * See:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols#iterator
+ */
+export type MoveIteratorRetType = {done?: boolean, value?: any};
+
+
+/**
+ * Implements the iterator protocol for List.
+ */
+export class Iterator<ValueType> {
+    private _curNode: DLNode<ValueType>;
+    private _endNode: DLNode<ValueType>;
+
+
+    constructor(curNode: DLNode<ValueType>, endNode: DLNode<ValueType>) {
+        this._curNode = curNode;
+        this._endNode = endNode;
+    }
+
+
+    public _getDLNode(): DLNode<ValueType> {
+        return this._curNode;
+    }
+
+
+    public equals(otherIter: Iterator<ValueType>): boolean {
+        return this._curNode === otherIter._curNode;
+    }
+
+
+    public next(): MoveIteratorRetType {
+        const retVal: MoveIteratorRetType = {};
+
+        const isDone: boolean = this._isAtEnd();
+        retVal.done = isDone;
+
+        if (!isDone) {
+            retVal.value = this._curNode.value;
+            this._curNode = this._curNode.next;
+        }
+
+        return retVal;
+    }
+
+
+    public get value(): ValueType {
+        if (this._isAtEnd()) {
+            return undefined;
+        } else {
+            return this._curNode.value;
+        }
+    }
+
+
+    private _isAtEnd(): boolean {
+        return this._curNode === this._endNode;
+    }
+}
+
